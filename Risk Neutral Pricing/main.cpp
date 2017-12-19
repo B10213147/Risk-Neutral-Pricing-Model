46692ec730af104eb58b4ebd1c005fef329d9148
B10213147 <B10213147@yuntech.org.tw>
2017-12-19 10:22:18 +0800

    Changed back to dynamic array

--- Risk Neutral Pricing/main.cpp
+++ Risk Neutral Pricing/main.cpp
@@ -1,14 +1,26 @@
+/*
+ * main.c
+ *
+ *  Created on: December 19, 2017
+ *      Author: Harvard Tseng
+ */
+
+/* Includes ------------------------------------------------------------------*/
 #include <iostream>
+#include <cstdlib>
 #include <cmath>
 
-#define     MAXSIZE     100 + 1
-
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
 using namespace std;
 
-void forward_Price(float S0, float u, float d, int vDate);
-float backward_Payoff(float p, float r, float K, int vDate);
+/* Private macro -------------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+double *forward_Price(float S0, float u, float d, int vDate);
+double backward_Payoff(double *price, float p, float r, float K, int vDate);
 
-float last_S[MAXSIZE];
+/* Private variables ---------------------------------------------------------*/
+
 int main()
 {
     float p = 0.5; // Up's Probability
@@ -40,43 +52,52 @@ int main()
     cout << "Valid date:"; cin >> vDate;
     */
     cout << "=====Forward S=====" << endl;
-    forward_Price(S0, u, d, vDate);
+    double *last_S = forward_Price(S0, u, d, vDate);
 
     cout << "=====Backward V=====" << endl;
-    float V0 = backward_Payoff(p, r, K, vDate);
+    float V0 = backward_Payoff(last_S, p, r, K, vDate);
     cout << "V0=" << V0 << endl;
 
+    free(last_S);
     return 0;
 }
 
+/* Private functions ---------------------------------------------------------*/
 /**
   * @brief  Calculate the price at valid date.
   * @param  S0: Starting price.
   * @param  u: Up pricing rate.
   * @param  d: Down pricing rate.
   * @param  vDate: Valid date.
-  * @retval None.
+  * @retval Pointer to the last S.
   */
-void forward_Price(float S0, float u, float d, int vDate){
+double *forward_Price(float S0, float u, float d, int vDate){
+    // Apply a vDate+1 spaces array for last date.
+    double *last_S = (double*)malloc(sizeof(double) * (vDate + 1));
+
     for(int i = 0; i < vDate + 1; i++){
         last_S[i] = S0 * pow(d, i) * pow(u, vDate - i);
     }
+
+    return last_S;
 }
 
 /**
   * @brief  Calculate the payoff of the option.
+  * @param  price: Pointer to price on valid date.
   * @param  p: Up's probability.
   * @param  r: Interest rate.
   * @param  K: Expected price.
   * @param  vDate: Valid date.
   * @retval V0.
   */
-float backward_Payoff(float p, float r, float K, int vDate){
+double backward_Payoff(double *price, float p, float r, float K, int vDate){
     float q = 1.0 - p;  // Down's probability
-    float cur_V[MAXSIZE];
+    // Apply a vDate+1 spaces array for last date.
+    double *cur_V = (double*)malloc(sizeof(double) * (vDate + 1));
 
     for(int i = 0; i < vDate + 1; i++){
-        float Vn = last_S[i] - K;  // Vn = (Sn - K).
+        float Vn = price[i] - K;  // Vn = (Sn - K).
         if(Vn < 0) Vn = 0;
         cur_V[i] = Vn;
     }
